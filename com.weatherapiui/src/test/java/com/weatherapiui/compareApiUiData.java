bf7249a464230e3cc28af1b0f4163d32ad1d38db
Sidharth <msidharth2018@gmail.com>
2020-12-11 06:46:45 +0530

    1) Testcase for fetching Temperature from api is added 2) Testcase for comparign temperature from api and UI is added.

--- com.weatherapiui/src/test/java/com/weatherapiui/compareApiUiData.java
+++ com.weatherapiui/src/test/java/com/weatherapiui/compareApiUiData.java
@@ -1,10 +1,23 @@
 package com.weatherapiui;
 
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -18,6 +31,7 @@ public class compareApiUiData {
 	String city="Mumbai";
 	WebDriver driver;
 	ArrayList<WebElement> citiesSearchResult;
+	int uiTemperature,apiTemperature;
   @BeforeClass
   public void beforeClass() {
 	  
@@ -32,7 +46,7 @@ public class compareApiUiData {
 	  driver.findElement(By.xpath("//input[@id='searchBox' and @class='searchBox' and @type='text']"))
 	  .sendKeys(city);
 	  citiesSearchResult=(ArrayList)driver.findElements(By.cssSelector("div[class='message']:not([style='display: none;'])"));
-	  Assert.assertTrue(citiesSearchResult.size()>0," Atlease one city with name should exist");
+	  Assert.assertTrue(citiesSearchResult.size()>0," Atleast one city with name should exist");
 	  
   }
   @Test(dependsOnMethods = "test1SearchCities")
@@ -60,13 +74,32 @@ public class compareApiUiData {
   }
   
   @Test(dependsOnMethods = "test2PinCity")
-  public void getTemperature() {
+  public void getUiTemperature() {
 	  
 	  WebElement temprature=driver.findElement(By.xpath("//*[contains(text(),'Temp in Degrees')]"));
 	  String[] tempStrings=temprature.getText().split(": ");
-	  Integer tempDegree=Integer.parseInt(tempStrings[1]);
-	  System.out.println(tempDegree);
+	  uiTemperature=Integer.parseInt(tempStrings[1]);
 	  
+  }
+  @Test
+  public void getApiTemperature() {
+	  
+	  RestAssured.baseURI="http://api.openweathermap.org/data/2.5/weather";
+	  RequestSpecification apiCall=RestAssured.given()
+			  .param("q",city)
+			  .param("appid","7fe67bf08c80ded756e598d6f8fedaea")
+			  .param("units","metric");
+	 
+	  Response apiResponse=apiCall.request(Method.GET);
+	   HashMap<Object,Object> weatherData=apiResponse.jsonPath().getJsonObject("main");
+	  apiTemperature=(Integer)weatherData.get("temp");
+	  System.out.println(apiTemperature);
+  }
+  
+  @Test(dependsOnMethods = {"getApiTemperature","getUiTemperature"})
+  public void compareApiUiTemperature() {
+	  System.out.println(uiTemperature+ " "+apiTemperature);
+	  Assert.assertTrue(Math.abs(uiTemperature-apiTemperature)<5,"The Temperature difference is acceptible");
   }
   
   @AfterClass
